aa2a931cf537e8cbec47632b4aa2f3a9d225eda6
ceipaha <ceip.aha@gmail.com>
2026-04-04 00:38:00 +0530

    Initial commit - added project 7

---
+++ project2/script.js
@@ -0,0 +1,44 @@
+const toggleBtn = document.getElementById('themeToggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('light-theme');
+  localStorage.setItem(
+    'theme', 
+    document.body.classList.contains('light-theme') ? 'light' : 'dark'
+    );
+    const isLight = document.body.classList.contains("light-theme");
+    toggleBtn.textContent = isLight ? "🌞":"🌙";
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Developer", "Designer", "Freelancer", "Student"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; 
+let textArrayIndex = 0;
+let charIndex = 0;      
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }else{
+        setTimeout(erase, newTextDelay);    
+    }
+}
+
+function erase() {
+    if(charIndex > 0){
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }else{
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (textArray.length) setTimeout(type, newTextDelay);
+});
